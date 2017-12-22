5679628d2ba4abe9741bea1e65e4ae58b30c881a
EchoUtopia <hero-blood@qq.com>
2017-12-22 19:50:34 +0800

    Update parse_bookmarks_to_json.go

--- parse_bookmarks_to_json.go
+++ parse_bookmarks_to_json.go
@@ -5,9 +5,11 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"io"
 	"os"
+	"flag"
 	//"encoding/base64"
 	"encoding/json"
 	"strconv"
+	"github.com/go-redis/redis"
 )
 
 type BookMark struct {
@@ -23,29 +25,49 @@ type BookMarkFolder struct {
 	AddDate         uint64            `json:"add_date"`
 	LastMofified    uint64            `json:"last_modified"`
 	PersonalToolBar string            `json:"personal_tool_bar"`
+	Level uint32			`json:"level"`
 	BookMarks       []*BookMark       `json:"bookmarks"`
 	ChildrenFolders []*BookMarkFolder `json:"children"`
 }
 
-func walk_folder(root *goquery.Selection, book_mark_folder *BookMarkFolder) {
+func walk_folder(root *goquery.Selection, book_mark_folder *BookMarkFolder, max_depth int) {
 
 	root.ChildrenFiltered("dt").Each(func(i int, selection *goquery.Selection) {
 		h3 := selection.ChildrenFiltered("h3")
 		//fmt.Println(h3.Nodes == nil)
 		if h3.Nodes != nil {
-			new_folder := new(BookMarkFolder)
-			add_date, _ := h3.Attr("add_date")
-			last_modified, _ := h3.Attr("last_modified")
-			personal_tool_bar, _ := h3.Attr("personal_tool_bar")
-			name := h3.Text()
-			//fmt.Println(name)
-			new_folder.Name = name
-			new_folder.AddDate, _ = strconv.ParseUint(add_date, 10, 32)
-			new_folder.LastMofified, _ = strconv.ParseUint(last_modified, 10, 32)
-			new_folder.PersonalToolBar = personal_tool_bar
-			book_mark_folder.ChildrenFolders = append(book_mark_folder.ChildrenFolders, new_folder)
-			dl := selection.ChildrenFiltered("dl").First()
-			walk_folder(dl, new_folder)
+			if int(book_mark_folder.Level) >= max_depth{
+				selection.Find("a").Each(func(i int, selection *goquery.Selection) {
+
+					url, _ := selection.Attr("href")
+					add_date, _ := selection.Attr("add_date")
+					//icon,_ := href.Attr("icon")
+					title := selection.Text()
+					//fmt.Println(title)
+					book_mark := new(BookMark)
+					book_mark.Url = url
+					//book_mark.Icon = icon
+					book_mark.Title = title
+					book_mark.AddDate, _ = strconv.ParseUint(add_date, 10, 32)
+					book_mark_folder.BookMarks = append(book_mark_folder.BookMarks, book_mark)
+				})
+			}else{
+				new_folder := new(BookMarkFolder)
+				add_date, _ := h3.Attr("add_date")
+				last_modified, _ := h3.Attr("last_modified")
+				personal_tool_bar, _ := h3.Attr("personal_tool_bar")
+				name := h3.Text()
+				//fmt.Println(name)
+				new_folder.Name = name
+				new_folder.AddDate, _ = strconv.ParseUint(add_date, 10, 32)
+				new_folder.LastMofified, _ = strconv.ParseUint(last_modified, 10, 32)
+				new_folder.PersonalToolBar = personal_tool_bar
+				new_folder.Level = book_mark_folder.Level + 1
+				book_mark_folder.ChildrenFolders = append(book_mark_folder.ChildrenFolders, new_folder)
+				dl := selection.ChildrenFiltered("dl").First()
+				walk_folder(dl, new_folder, max_depth)
+
+			}
 		} else {
 			href := selection.ChildrenFiltered("a").First()
 			url, _ := href.Attr("href")
@@ -65,38 +87,65 @@ func walk_folder(root *goquery.Selection, book_mark_folder *BookMarkFolder) {
 
 }
 
-func ParseBookmarks(file io.Reader) (*BookMarkFolder, error) {
+func ParseBookmarks(file io.Reader, max_depth int) (*BookMarkFolder, error) {
 	doc, err := goquery.NewDocumentFromReader(file)
 	if err != nil {
 		fmt.Println("haha",err)
 		return nil, err
 	}
 	book_mark_folder := new(BookMarkFolder)
-	root_title := doc.Find("H1").Text()
+	book_mark_folder.Level = 0
+	root := doc.Find("DL").First()
+	root_title := root.Find("h3").First().Text()
+	//fmt.Println(root_title)
 	book_mark_folder.Name = root_title
-	dl := doc.Find("DL").First()
+	dl := root.Find("dl").First()
+	//fmt.Println(dl.Text())
 	//doc.Children().Each(func(i int, selection *goquery.Selection){
 	//	//fmt.Println(selection.Nodes)
 	//	fmt.Println(i)
 	//	fmt.Println(selection.Text())
 	//	//fmt.Println(selection.ChildrenFiltered("a").Text())
 	//})
-	walk_folder(dl, book_mark_folder)
+	walk_folder(dl, book_mark_folder, max_depth)
 	return book_mark_folder, nil
 }
 
+func NewClient() *redis.Client{
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:13379",
+		Password: "",
+		DB: 0,
+	})
+	//pong, err := client.Ping().Result()
+	//fmt.Println(pong, err)
+	return client
+}
+
 func main() {
-	file, err := os.Open("/home/shaoxing/Documents/bookmarks_12_20_17.html")
+	var (
+		path string
+		redis_key string
+		user_id string
+		max_depth int
+	)
+	flag.StringVar(&path, "path", "", "a string")
+	flag.StringVar(&redis_key, "redis_key", "", "a string")
+	flag.StringVar(&user_id, "user_id", "", "a string")
+	flag.IntVar(&max_depth, "max_depth", 1, "an int")
+	flag.Parse()
+
+	file, err := os.Open(path)
 	if err != nil {
 		fmt.Println("sdf", err)
 	}
 	defer file.Close()
-	book_mark_folder, _ := ParseBookmarks(file)
+	book_mark_folder, _ := ParseBookmarks(file, max_depth)
 	b, err := json.MarshalIndent(book_mark_folder, "", "    ")
 	if err != nil {
 		fmt.Println("error", err)
 	}
-	//fmt.Printf("%T", book_mark_folder)
-	os.Stdout.Write(b)
-	//fmt.Printf("%T",b)
+	redis_client := NewClient()
+	redis_client.HSet(redis_key, user_id, b)
+
 }
