606920e0970f0e2abb0d0c99df30ab75d279e0f4
EchoUtopia <hero-blood@qq.com>
2018-08-27 17:53:54 +0800

    Create gin-middleware-demo.go

---
+++ gin-middleware-demo.go
@@ -0,0 +1,52 @@
+package main
+
+type context struct {
+	index int
+	handlers [] func(*context)
+}
+
+func NewContext() *context{
+	c := new(context)
+	c.index = -1
+	c.handlers = make([]func(*context), 0, 10)
+	return c
+}
+
+func (c *context) Next(){
+	c.index ++
+	for l := len(c.handlers);c.index < l;c.index++{
+		c.handlers[c.index](c)
+	}
+}
+
+func (c *context)AddHandler(f func (*context)){
+	c.handlers = append(c.handlers, f)
+}
+
+func middleware1(c *context){
+	println("middleware1")
+}
+
+func middleware2(c *context){
+	println("middleware2 pre")
+	c.Next()
+	println("middleware2 post")
+}
+
+func middleware3(c *context){
+	c.Next()
+	println("middleware3")
+}
+
+func handler(c *context){
+	println("handler")
+}
+
+func main() {
+	c := NewContext()
+	c.AddHandler(middleware1)
+	c.AddHandler(middleware2)
+	c.AddHandler(middleware3)
+	c.AddHandler(handler)
+	c.Next()
+}
