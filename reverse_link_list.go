a9fde64883dcf3ea5939640ecf755a434e458c79
EchoUtopia <hero-blood@qq.com>
2018-12-28 16:01:40 +0800

    Update reverse_link_list.go

--- reverse_link_list.go
+++ reverse_link_list.go
@@ -4,56 +4,37 @@ import (
 	"fmt"
 )
 
-type LinkNode struct {
-	value int
-	next *LinkNode
+type Node struct {
+	Val  int
+	Next *Node
 }
 
-func CreateList(a []int) *LinkNode{
-	head := new(LinkNode)
-	head.value = a[0]
-	tmp := head
-	for _, i := range a[1:] {
-		next := new(LinkNode)
-		next.value = i
-		tmp.next = next
-		tmp = next
+func (n *Node) print() {
+	out := ""
+	for n != nil {
+		out += fmt.Sprintf(`%d-> `, n.Val)
+		n = n.Next
 	}
-	return head
+	fmt.Println(out)
 }
 
-func ReverseList(head *LinkNode) *LinkNode{
-	if head == nil || head.next == nil{
-		return head
+func reverse(list *Node) *Node {
+	if list.Next == nil || list == nil {
+		return list
 	}
-	cur := head.next
-	pre := head
-	head.next = nil
-	for cur != nil{
-		next := cur.next
-		cur.next = pre
-		pre = cur
-		cur = next
+	pre, cur := list, list.Next
+	pre.Next = nil
+	for cur != nil {
+		next := cur.Next
+		cur.Next = pre
+		pre, cur = cur, next
 	}
 	return pre
-
 }
 
-
-func ThroughList(head *LinkNode){
-	str := ""
-	node := head
-	for node != nil{
-		str += fmt.Sprintf("->%d", node.value)
-		node = node.next
-	}
-	println(str)
-}
-
-func main(){
-	node := CreateList([]int{1,2,3,4,5})
-	ThroughList(node)
-	rev := ReverseList(node)
-	fmt.Printf("%#v\n", rev)
-	ThroughList(rev)
+func main() {
+	a := &Node{1, &Node{2, &Node{3, nil}}}
+	a.print()
+	b := reverse(a)
+	b.print()
 }
